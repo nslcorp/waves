4f30bd5c8de69fdc9850913d4f7381213e797bc1
Serhii_Nikitin <Vverbatim123>
2018-09-27 16:34:37 +0300

    starting with AddProduct

---
+++ client/src/components/user/admin/add-product/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UserLayout from '../../../dashboard/user-layout';
+
+const AddProduct = props => (
+  <UserLayout>
+    <div>
+      <h3>AddProduct</h3>
+    </div>
+  </UserLayout>
+);
+
+AddProduct.propTypes = {};
+
+export default AddProduct;
