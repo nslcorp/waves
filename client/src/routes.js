02590cea883b99428dcae592121bd161dc532127
Serhii_Nikitin <Vverbatim123>
2018-10-09 13:36:13 +0300

    remove relative paths

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Layout from 'components/layout';
-import Home from 'components/home';
+import Layout from './components/layout';
+import Home from './components/home';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
 import Dashboard from './components/dashboard';
