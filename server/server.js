fefea04257b8e261d207bfcc0ca47280886c148f
Serhii_Nikitin <Vverbatim123>
2018-10-09 10:47:14 +0300

    add changes to deploy to Heroku

--- server/server.js
+++ server/server.js
@@ -23,7 +23,6 @@ cloudinary.config({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static('client/build'));
 
 // app.use('/api', routes);
 
@@ -33,8 +32,10 @@ require('./routes/woodRoutes')(app);
 require('./routes/productRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
   const path = require('path');
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendfile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
